888f39e142dc1dae2315d9c9ba8613fc6835f5bb
angel-arciniega <angel_arciniega@uaeh.edu.mx>
2025-04-25 23:42:39 -0600

    Delete utils directory

--- utils/truncate_string.py
+++
@@ -1,7 +0,0 @@
-# For media captions
-
-
-def truncate_string(input_string: str, max_length: int = 1024) -> str:
-    if len(input_string) > max_length:
-        return input_string[:max_length]
-    return input_string
